fa06e7d8da546b3f8955b4db9a716c6941e0d67c
Hyunju Lee <guswnee00@gmail.com>
2024-04-03 21:22:32 +0900

    show banner at main page

--- src/main/java/com/zerobase/fastlms/main/controller/MainController.java
+++ src/main/java/com/zerobase/fastlms/main/controller/MainController.java
@@ -1,35 +1,29 @@
 package com.zerobase.fastlms.main.controller;
 
-
-import com.zerobase.fastlms.components.MailComponents;
+import com.zerobase.fastlms.admin.dto.BannerDto;
+import com.zerobase.fastlms.admin.model.BannerParam;
+import com.zerobase.fastlms.admin.service.BannerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class MainController {
 
-    private final MailComponents mailComponents;
+    private final BannerService bannerService;
 
-    //TODO
-    // -메인화면에 배너표시
     @RequestMapping("/")
-    public String index() {
-        
-        /*
-        String email = "satcop@naver.com";
-        String subject = " 안녕하세요. 제로베이스 입니다. ";
-        String text = "<p>안녕하세요.</p><p>반갑습니다.</p>";
-        
-        mailComponents.sendMail(email, subject, text);
-        */
-        
+    public String index(Model model, BannerParam param) {
+        List<BannerDto> bannerDtoList = bannerService.frontList(param);
+        model.addAttribute("list", bannerDtoList);
         return "index";
     }
     
-    
-    
+
     @RequestMapping("/error/denied")
     public String errorDenied() {
         
